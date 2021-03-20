fc01b2c2c087ca01fe8372a241e2c83c2585f924
Karo Lencina <karolencina@Karolinas-Air.home>
2021-03-20 21:13:23 +0100

    a card component created with React.js, inspired by Pure React book

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const testCard = {
+  bank: "Big Bank, Inc.",
+  name: "CARDHOLDER NAME",
+  expDate: "19-08",
+  cardNum: "1234 5678 8765 4321"
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const CreditCard = ({ cardInfo }) => {
+  return (
+    <div className="card">
+      <Bank bankName={cardInfo.bank}/>
+      <AccountNum number={cardInfo.cardNum}/>
+      <div className="nums">1234</div>
+      <div className ="valid-thru">
+        <div>VALID</div>
+        <div>THRU</div>
+      </div>
+      <Expiration date={cardInfo.expDate}/>
+      <Name name={cardInfo.name}/>
+    </div>
+  )
+};
+
+const Name = ({ name }) => <div className="cardholder">{name}</div>;
+
+const Bank = ({ bankName }) => <div className="bank-name">{bankName}</div>; 
+
+const AccountNum = ({ number }) => <div className="account-num">{number}</div>; 
+
+const Expiration = ({ date }) => <div className="exp-date">{date}</div>;
+
+ReactDOM.render(<CreditCard cardInfo={testCard}/>, document.getElementById('root'));
